e0c28353cf0e0b79e283744c0db1cf120f10de93
Waszko <tom.waszkowycz@hotmail.co.uk>
2014-12-23 15:25:37 +0000

    moved IO stuff from fol.ml to io.ml

--- parse/fol.ml
+++ parse/fol.ml
@@ -1,35 +1,9 @@
-(* write dimacs_cnf string to "out.cnf" file *)
-let write_cnf str =
-    let oc = open_out "out.cnf" in
-    Printf.fprintf oc "%s" str;
-    close_out oc
-
 (* call minisat on "out.cnf", storing result in "out.txt" *)
 let call_minisat () =
     print_string( "Minisat exit code: " ^ string_of_int (
         Sys.command "../sat_solvers/minisat/minisat out.cnf out.txt" ) );
     print_newline();
     flush stdout
-
-(* read out.txt to find satisfying assignment (or not) *)
-let output_answer () =
-    let ic = open_in "out.txt" in
-    try 
-        let line = input_line ic in
-        if line = "SAT" then (
-            let line2 = input_line ic in
-         (* let vars = Str.split (Str.regexp " ") line in *)
-         (* map back to predicates?? *)
-            print_string ("Satisfying assignment: " ^ line2 ^ "\n") )
-        else if line = "UNSAT" then (
-            print_string "No satisfying assignment exists\n"; )
-        else
-            print_string "FILE FORMAT ERROR\n"; (* throw error? *)
-        flush stdout;    
-        close_in ic       
-    with e ->              
-        close_in_noerr ic;  
-        raise e              
 
 let _ =
     let problem = open_in "3col.txt" in
@@ -44,7 +18,7 @@ let _ =
     let cnf = Cnf.cnf_expr subbed in
     print_newline(); flush stdout;
     let dimacs = Dimacs.dimacs_of_expr_call cnf nbvars in
-    write_cnf dimacs;
+    Io.write_cnf dimacs;
     
     call_minisat ();
-    output_answer ();
+    Io.output_answer ();
--- parse/io.ml
+++ parse/io.ml
@@ -1,5 +1,6 @@
 module String_map = Map.Make (String);;
 
+(* reads instance sets into map from name to string list list *)
 let read_instance () =
     let ic = open_in "instance.txt" in
     let sets_map = ref String_map.empty in (* map from set name to set *)
@@ -15,18 +16,30 @@ let read_instance () =
         done; !sets_map
     with End_of_file ->
         close_in ic;
-        !sets_map (* !? *)
+        !sets_map 
 
+(* write dimacs_cnf string to "out.cnf" file *)
+let write_cnf str =
+    let oc = open_out "out.cnf" in
+    Printf.fprintf oc "%s" str;
+    close_out oc
 
-(* input.txt looks like:
-V 1
-V 2
-V 3
-V 4
-E 1 3
-E 1 4
-E 3 4
-
-from this we want 2 lists of lists:
-V = [["1"];["2"];["3"];["4"]]
-E = [["1";"3"];["1";"4"];["3";"4"]] *)
+(* read out.txt to find satisfying assignment (or not) *)
+let output_answer () =
+    let ic = open_in "out.txt" in
+    try 
+        let line = input_line ic in
+        if line = "SAT" then (
+            let line2 = input_line ic in
+         (* let vars = Str.split (Str.regexp " ") line in *)
+         (* map back to predicates?? *)
+            print_string ("Satisfying assignment: " ^ line2 ^ "\n") )
+        else if line = "UNSAT" then (
+            print_string "No satisfying assignment exists\n"; )
+        else
+            print_string "FILE FORMAT ERROR\n"; (* throw error? *)
+        flush stdout;    
+        close_in ic       
+    with e ->              
+        close_in_noerr ic;  
+        raise e              
